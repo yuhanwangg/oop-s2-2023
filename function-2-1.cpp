392e78f74e63b344401f55ce8191162f04ab9206
Yuhan Wang <wyuhan18@gmail.com>
2023-07-27 15:59:53 +0930

    for 2

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -4,7 +4,7 @@
 
 int min_element(int array[], int n) {
   if (n > 0) {
-    int min = array[1];
+    int min = array[0];
     for (int i = 1; i < n; i++) {
       if (array[i] < min) {
         min = array[i];
