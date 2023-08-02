2e910c5dfa5c9f26dbe1332668f3fd7e7276359b
Yuhan Wang <wyuhan18@gmail.com>
2023-08-02 13:21:36 +0930

    prac 2 1

---
+++ function-1-1.cpp
@@ -0,0 +1,14 @@
+/* Given a matrix of integers, return the sum of the elements along 
+the main diagonal (top left bottom right)*/
+
+int sum_diagonal(int array[4][4]) {
+    int sum = 0;
+    for (int i = 0; i<4; i++){
+        for (int j = 0; j<4; j++){
+            if (i == j){
+                sum = sum + array[i][j];
+            }
+        }
+    }
+    return sum;
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+extern int sum_diagonal(array[4][4]);
+
+int main(void){
+    int array[4][4] = {};
+    for (int i = 0; i<4; i++){
+        for (int j = 0; j<4; j++){
+            array[i][j] = 1;
+        }
+    }
+    cout << sum_diagonal(array[4][4]) << endl;
+}
