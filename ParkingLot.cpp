484f55bc4f1184f823ba02c1596b84f4b299aac2
Yuhan Wang <wyuhan18@gmail.com>
2023-08-30 15:50:21 +0930

    1-2 prac

---
+++ ParkingLot.cpp
@@ -0,0 +1,39 @@
+
+#include "ParkingLot.h"
+
+#include <iostream>
+
+#include "Vehicle.h"
+using namespace std;
+#include <string.h>
+
+ParkingLot::ParkingLot() {
+  max_capacity = 0;
+  count = 0;
+};
+ParkingLot::ParkingLot(int max_capacity) {
+  this->max_capacity = max_capacity;
+  vehicles = new Vehicle(max_capacity);
+  count = 0;
+};
+int ParkingLot::getCount() { return count; };
+void ParkingLot::parkVehicle(Vehicle vehicle) {
+  if (count <= max_capacity) {
+    vehicles[count] = vehicle;
+    count++;
+  } else {
+    cout << "The lot is full" << endl;
+  }
+};
+void ParkingLot::unparkVehicle(int ID) {
+  for (int i = 0; i < count; i++) {
+    if (vehicles[i].getID() == ID) {
+      vehicles[i] = NULL;
+    } else {
+      cout << "Vehicle not in lot" << endl;
+    }
+  }
+};
+ParkingLot::~ParkingLot(){
+
+};
---
+++ ParkingLot.h
@@ -0,0 +1,25 @@
+#ifndef PARKINGLOT_H
+#define PARKINGLOT_H
+#include <iostream>
+
+#include "Vehicle.h"
+using namespace std;
+#include <string.h>
+
+class ParkingLot {
+ protected:
+  Vehicle* vehicles;
+  int max_capacity;
+  int count;
+
+ public:
+  ParkingLot();
+  ParkingLot(int max_capacity);
+  int getCount();
+  void parkVehicle(Vehicle vehicle);
+  void unparkVehicle(int ID);
+
+  ~ParkingLot();
+};
+
+#endif
