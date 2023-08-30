09a8653c0476fab0184362fe882ca6c1187b4f9f
Yuhan Wang <wyuhan18@gmail.com>
2023-08-30 16:15:49 +0930

    prac4

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -10,18 +10,19 @@ using namespace std;
 int main(void) {
   // number of vehicles
   ParkingLot parking(5);
-  Vehicle v1(29384);
+  Vehicle v(2934);
+  Vehicle* v1 = &v;
   Vehicle v2(14356);
   Vehicle v3(90680);
   Vehicle v4(27938);
   Vehicle v5(38902);
   Vehicle v6(32859);
   parking.parkVehicle(v1);
-  parking.parkVehicle(v2);
+  /*parking.parkVehicle(v2);
   parking.parkVehicle(v3);
   parking.getCount();
   parking.parkVehicle(v4);
   parking.parkVehicle(v5);
   parking.parkVehicle(v6);
-  parking.unparkVehicle(38902);
+  parking.unparkVehicle(38902);*/
 }
