95459c2acd99a4368048ee7fb18d29605861e546
Yuhan Wang <wyuhan18@gmail.com>
2023-08-31 14:54:14 +0930

    EDited main

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
 #include "Bus.h"
 #include "Car.h"
@@ -10,47 +10,39 @@ using namespace std;
 int main(void) {
   // number of vehicles
 
-  int nCars = 6;
-  int nBuses = 2;
-  int nMotorbikes = 2;
-  int sumVehicles = nCars + nBuses + nMotorbikes;
-  ParkingLot parking = ParkingLot(sumVehicles);
-  // Vehicle v(2934);
-  // Vehicle* v1 = &v;
-  // Vehicle v22(38204);
-  // Vehicle* v2 = &v22;
-  // Vehicle v2(14356);
-  // Vehicle v3(90680);
-  // Vehicle v4(27938);
-  // Vehicle v5(38902);
-  // Vehicle v6(32859);
-  // parking.parkVehicle(v1);
-  // parking.parkVehicle(v2);
+  std::string vehicleType;
+  ParkingLot lot(10);
 
-  int ID_count = 0;
-  for (int i = 0; i < nCars; i++) {
-    parking.parkVehicle(new Car(ID_count++));
-  }
-  for (int i = 0; i < nBuses; i++) {
-    parking.parkVehicle(new Bus(ID_count++));
-  }
-  for (int i = 0; i < nMotorbikes + 1; i++) {
-    parking.parkVehicle(new Motorbike(ID_count++));
-  }
+  bool full = false;
+  int count = 0;
 
-  cout << parking.getCount() << "\n";
-  parking.unparkVehicle(1);
-  parking.unparkVehicle(3);
-  parking.unparkVehicle(30);
-  cout << parking.getCount() << "\n";
-  parking.printVehicle();
-  cout << endl;
+  while (full == false) {
+    std::cout << "What vehicle type?" << std::endl;
+    std::cin >> vehicleType;
 
-  /*parking.parkVehicle(v2);
-  parking.parkVehicle(v3);
-  parking.getCount();
-  parking.parkVehicle(v4);
-  parking.parkVehicle(v5);
-  parking.parkVehicle(v6);
-  parking.unparkVehicle(38902);*/
+    if (vehicleType == "c" || vehicleType == "C") {
+      Car newVehicle = Car(count);
+      lot.parkVehicle(&newVehicle);
+      count++;
+      std::cout << "Car parked";
+    } else if (vehicleType == "b" || vehicleType == "B") {
+      Bus newVehicle = Bus(count);
+      lot.parkVehicle(&newVehicle);
+      count++;
+      std::cout << "Bus parked";
+    } else if (vehicleType == "m" || vehicleType == "M") {
+      Motorbike newVehicle = Motorbike(count);
+      lot.parkVehicle(&newVehicle);
+      count++;
+      std::cout << "Motorbike parked";
+    }
+    if (lot.getCount() == 10) {
+      full = true;
+    }
+  }
+  int unparkID = 0;
+
+  std::cout << "Enter an ID to remove" << std::endl;
+  std::cin >> unparkID;
+  lot.unparkVehicle(unparkID);
 }
