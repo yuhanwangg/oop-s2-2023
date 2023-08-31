b0b728de24f2619d9be96486ef2940eddedb13bc
Yuhan Wang <wyuhan18@gmail.com>
2023-08-31 12:38:00 +0930

    prac

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -20,6 +20,7 @@ int ParkingLot::getCount() { return count; };
 void ParkingLot::parkVehicle(Vehicle* vehicle) {
   if (count <= max_capacity) {
     vehicles[count] = *vehicle;
+    // std::cout << "Parking successful" << std::endl;
     count++;
   } else {
     std::cout << "The lot is full" << std::endl;
@@ -29,6 +30,10 @@ void ParkingLot::unparkVehicle(int ID) {
   for (int i = 0; i < count; i++) {
     if (vehicles[i].getID() == ID) {
       vehicles[i] = NULL;
+      vehicles[0] = vehicles[1];
+      for (int i = 0; i < count; i++) {
+        vehicles[i - 1] = vehicles[i];
+      }
     } else {
       std::cout << "Vehicle not in lot" << std::endl;
     }
