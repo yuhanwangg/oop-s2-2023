0fef28222eb0d6c34d587f1956811a2e1bcb69be
Yuhan Wang <wyuhan18@gmail.com>
2023-09-07 14:49:40 +0930

    ALMOST

--- Fleet.cpp
+++ Fleet.cpp
@@ -9,10 +9,15 @@ using namespace std;
 
 Fleet::Fleet() {
   fleet = new Car*[5];
-  fleet[1] = new Tesla('S', 10000);
-  fleet[2] = new Ford(1, 20000);
-  fleet[3] = new Car(30000);
-  fleet[4] = new Ford(2, 40000);
-  fleet[5] = new Tesla('T', 50000);
+  fleet[1] = new Tesla[1];
+  fleet[2] = new Ford[1];
+  fleet[3] = new Car[1];
+  fleet[4] = new Ford[1];
+  fleet[5] = new Tesla[1];
+  fleet[1] = &Tesla('S', 10000);
+  fleet[2] = &Ford(1, 20000);
+  fleet[2] = &Car(30000);
+  fleet[2] = &Ford(2, 40000);
+  fleet[2] = &Tesla('T', 50000)
 };
 Car** Fleet::get_fleet() { return fleet; };
