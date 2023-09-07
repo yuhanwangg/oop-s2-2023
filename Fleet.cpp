027054e9caf64b70a66f4c28bf70790678cdcf35
Yuhan Wang <wyuhan18@gmail.com>
2023-09-07 14:55:09 +0930

    FHIGSD

--- Fleet.cpp
+++ Fleet.cpp
@@ -8,16 +8,21 @@
 using namespace std;
 
 Fleet::Fleet() {
+  t1 = Tesla('S', 10000);
+  f1 = Ford(1, 20000);
+  c1 = Car(30000);
+  f2 = Ford(2, 40000);
+  t2 = Tesla('T', 50000);
   fleet = new Car*[5];
   fleet[1] = new Tesla[1];
   fleet[2] = new Ford[1];
   fleet[3] = new Car[1];
   fleet[4] = new Ford[1];
   fleet[5] = new Tesla[1];
-  fleet[1] = t1;
-  fleet[2] = f1;
-  fleet[3] = c1;
-  fleet[4] = f2;
-  fleet[5] = t2;
+  fleet[1] = &t1;
+  fleet[2] = &f1;
+  fleet[3] = &c1;
+  fleet[4] = &f2;
+  fleet[5] = &t2;
 };
 Car** Fleet::get_fleet() { return fleet; };
