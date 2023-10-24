c2b82b22edb0140e5d66556fc14bce1fc228b0ad
Yuhan Wang <wyuhan18@gmail.com>
2023-10-24 16:58:02 +1030

    my brain

--- Ship.h
+++ Ship.h
@@ -11,8 +11,8 @@ class Ship : public GameEntity {
   Ship(int x, int y) : GameEntity(x, y, 'S'){};
   Ship();
   void move(int dx, int dy) {  // setPosition(dx, dy);
-    std::get<0>(position) = dx;
-    std::get<1>(position) = dy;
+    std::get<0>(position) = std::get<0>(position) + dx;
+    std::get<1>(position) = std::get<1>(position) + dy;
   };
 };
 
