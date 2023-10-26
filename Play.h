dd213aa83a1b6f81af20fcbfb7580d6d014c719b
Yuhan Wang <wyuhan18@gmail.com>
2023-10-26 14:38:40 +1030

    please work

--- Play.h
+++ Play.h
@@ -61,48 +61,46 @@ class Play {
     int iterations = 0;
     int p = 0;
     int s = 0;
+    int i = 0;
     while (!finished) {
-      for (int i = 0; i < matrix.size(); i++) {
-        if (personaMatrix[p]->getCategory() == 'P' &&
-            p < personaMatrix.size()) {
-          std::cout << "successfully shifted from x= "
-                    << std::get<0>(matrix[i]->getLoc())
-                    << ",y= " << std::get<1>(matrix[i]->getLoc()) << "\n";
-          personaMatrix[p]->shift(1, 0);
+      if ((p < personaMatrix.size()) &&
+          (personaMatrix[p]->getCategory() == 'P')) {
+        personaMatrix[p]->shift(1, 0);
 
-          std::tuple<int, int> tempLoc = personaMatrix[p]->getLoc();
-          if (std::get<0>(tempLoc) > matrixWidth ||
-              std::get<0>(tempLoc) > matrixHeight) {
-            std::cout << "Persona has won the game!"
-                      << "\n";
-            winCondition = true;
-            finished = true;
-          }
-
-          if (snareMatrix[s]->getCategory() == 'S' && s < snareMatrix.size()) {
-            std::cout << "snare found"
-                      << "\n";
-            if (Assists::evaluateDistance(personaMatrix[p]->getLoc(),
-                                          snareMatrix[s]->getLoc()) <
-                snareTriggerDistance)
-              snareMatrix[s]->implement(*personaMatrix[p]);
-            matrix[i]->setCategory('S');
-            std::cout << "persona is now: " << matrix[i]->getCategory() << "\n";
-          } else {
-            s++;
-          }
-        } else {
-          p++;
+        std::tuple<int, int> tempLoc = personaMatrix[p]->getLoc();
+        if (std::get<0>(tempLoc) > matrixWidth ||
+            std::get<0>(tempLoc) > matrixHeight) {
+          std::cout << "Persona has won the game!"
+                    << "\n";
+          winCondition = true;
+          finished = true;
         }
+
+        if ((s < snareMatrix.size()) &&
+            (snareMatrix[s]->getCategory() == 'S')) {
+          std::cout << "snare found"
+                    << "\n";
+          if (Assists::evaluateDistance(personaMatrix[p]->getLoc(),
+                                        snareMatrix[s]->getLoc()) <
+              snareTriggerDistance)
+            snareMatrix[s]->implement(*personaMatrix[p]);
+          matrix[p]->setCategory('S');
+
+        } else {
+          s++;
+        }
+      } else {
+        p++;
+        i++;
       }
 
       iterations++;  // checking iterations
-      std::cout << "iteration number: " << iterations << "\n";
       if (iterations > maxCycles) {
         std::cout << "Maximum number of cycles reached. Game over."
                   << "\n";
         finished = true;
       }
+      std::cout << "iteration number: " << iterations << "\n";
     }
   }
   // getter and setter
