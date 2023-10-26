ddbb4ea298fa0b80f9619265064e7f9bb7dea706
Yuhan Wang <wyuhan18@gmail.com>
2023-10-26 12:57:09 +1030

    nIKAJND

--- Utils.h
+++ Utils.h
@@ -8,14 +8,14 @@
 class Utils {
  protected:
  public:
-  std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight) {
+  static std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight) {
     int x = rand() % gridWidth;
     int y = rand() % gridHeight;
     std::tuple<int, int> tempArray = std::make_tuple(x, y);
     return tempArray;
   }
-  double calculateDistance(std::tuple<int, int> pos1,
-                           std::tuple<int, int> pos2) {
+  static double calculateDistance(std::tuple<int, int> pos1,
+                                  std::tuple<int, int> pos2) {
     double distance;
     double sum1 = std::get<0>(pos1) - std::get<0>(pos2);
     double sum2 = std::get<1>(pos1) - std::get<0>(pos2);
